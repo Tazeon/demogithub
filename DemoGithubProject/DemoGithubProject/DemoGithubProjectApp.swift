e5fa06dfeeb985e6aea3c45d9a93e385e127b156
kamkon-nnn <catarogpong@gmail.com>
2024-06-22 20:04:38 -0700

    initial commit

---
+++ DemoGithubProject/DemoGithubProject/DemoGithubProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoGithubProjectApp.swift
+//  DemoGithubProject
+//
+//  Created by admin on 22/6/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct DemoGithubProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
