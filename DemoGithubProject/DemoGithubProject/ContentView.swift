a7f6c479eb2353f5d0a8bcd9659ecb9e8f2b1e6a
kamkon-nnn <90666002+Tazeon@users.noreply.github.com>
2024-06-22 20:25:04 -0700

    add file clone from another comp

--- DemoGithubProject/DemoGithubProject/ContentView.swift
+++ DemoGithubProject/DemoGithubProject/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Hello, world!")
             Text("This is Github Demo")
+            Text("Change file clone")
         }
         .padding()
     }
